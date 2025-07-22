8a62ce271902fb0ac76188e8d865f9e344996a9c
Abel García <46522725+AbelGRubio@users.noreply.github.com>
2025-07-22 10:07:31 +0200

    fix(Abel): added menuItems.js

---
+++ src/data/menuItems.js
@@ -0,0 +1,7 @@
+// src/data/menuItems.js
+export const menuItems = [
+  { name: 'About me', href: '/about' },
+  // { name: 'Portfolio', href: '/portfolio' },
+  { name: 'Blog', href: '/blog' },
+  // { name: 'Contact', href: '/contact' },
+];
