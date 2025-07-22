e22c99cd15ba6fc427e3ec3b7fef971dc8106fb7
Abel García <46522725+AbelGRubio@users.noreply.github.com>
2025-07-22 10:08:34 +0200

    fix(Abel): added .gitignore, astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://agrubio.dev',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [mdx(), sitemap()],
 });
